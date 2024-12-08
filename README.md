50aef49dedbdd2390a9b857b5db03d3dcf9730cf
grahamstelzer <graham.stelzer.01@gmail.com>
2024-12-08 15:49:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Discord eats up my ram so making a different communication service for my team and I to use.
+
+golang, fiber, pusher
+following tutorial, will likely be rebuilding several pieces
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "log"
+
+    "github.com/gofiber/fiber/v3"
+    "github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+    // Initialize a new Fiber app
+    app := fiber.New()
+
+    // backend run on 8000, frontend runs on different port
+    // why cors?
+    app.Use(cors.New()) 
+
+
+    // Define a route for the GET method on the root path '/'
+    app.Get("/", func(c fiber.Ctx) error {
+        // Send a string response to the client
+        return c.SendString("Hello, World 👋!")
+    })
+
+    // Start the server on port 3000
+    log.Fatal(app.Listen(":8000"))
+}
